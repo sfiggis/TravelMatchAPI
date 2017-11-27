ae447b70c072ccc45d214f317a2f4a602f36795a
Sophie Figgis <sophiefiggis@gmail.com>
2017-11-27 17:19:00 +0000

    fix namespacing

--- app/graphql/unions/codeable_union.rb
+++ app/graphql/unions/codeable_union.rb
@@ -2,6 +2,6 @@ module Unions
   CodeableUnion = GraphQL::UnionType.define do
     name 'CodeableUnion'
     description 'Codes for multiple types'
-    possible_types [LocationType, AirportType]
+    possible_types [Types::LocationType, Types::AirportType]
   end
 end
