af6967db274d85c9987c026f8fd54e3b1e114c7c
Sophie Figgis <sophiefiggis@gmail.com>
2017-11-27 17:17:58 +0000

    Test query graphql schema on location in graphiql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,9 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
+
+  field :locations,
+      function: Functions::List.new(model: Location)
+    field :location,
+      function: Functions::Find.new(model: Location)
+
 end
