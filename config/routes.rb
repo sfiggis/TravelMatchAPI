fc4cc0e2682d07862b5884ff29c71adaf380d5bb
Sophie Figgis <sophiefiggis@gmail.com>
2017-11-27 17:17:19 +0000

    Setup graphiql to check schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  post '/graphql', to: 'graphql#execute'
+  if Rails.env.development?
+    mount(
+      GraphiQL::Rails::Engine,
+      at: '/graphiql',
+      graphql_path: '/api/v2/queries'
+    )
+  end
+  post '/graphql', to: 'graphql#execute', as: 'graphql_path'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
